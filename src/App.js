ff266d962f9e712b3d083d6ceb76594fe6042f57
ishansharma-intellinez <ishan.sharma@intellinez.com>
2021-09-24 13:08:47 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz! dssd</h1>
+      <h1>Hello mededLink</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
